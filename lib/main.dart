13d1435abd2b3f82706fc1220ef60b65b3032551
aamartus <andronick@outlook.com>
2022-09-30 22:03:12 +0000

    SOC-34: Added Non-Functional Text Field to Project

--- lib/main.dart
+++ lib/main.dart
@@ -102,6 +102,13 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            const TextField(
+              obscureText: false,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Sample Text Field',
+              ),
+            )
           ],
         ),
       ),
